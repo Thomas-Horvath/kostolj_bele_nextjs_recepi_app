4f9e4baa640a0984e6197019fe78e7d827ac000d
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-08-21 18:57:14 +0200

    renew auth

--- app/api/favorites/route.js
+++ app/api/favorites/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../../pages/api/auth/[...nextauth].js"; 
+import { authOptions } from "../../../lib/auth"; 
 import { prisma } from "../../../lib/prisma";
 import { NextResponse } from "next/server";
 
